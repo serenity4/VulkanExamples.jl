d200c9070eda82c6c858a689d569d94a5d003eb6
serenity4 <cedric.bel@hotmail.fr>
2021-01-15 17:59:50 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/VulkanExamples.jl
@@ -0,0 +1,5 @@
+module VulkanExamples
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using VulkanExamples
+using Test
+
+@testset "VulkanExamples.jl" begin
+    # Write your tests here.
+end
