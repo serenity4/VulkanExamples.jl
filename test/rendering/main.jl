42c40282667acc281eda95f3a2f4a5bdacab8fe9
serenity4 <cedric.bel@hotmail.fr>
2021-03-13 19:30:13 +0100

    Fix vertices not being drawn

--- test/rendering/main.jl
+++ test/rendering/main.jl
@@ -57,8 +57,6 @@ colors = [
     Point4f(1., 1., 1., 0.4),
 ]
 
-vertex_indices = UInt32.(collect(1:4) .- 1)
-
 function find_memory_type(physical_device::PhysicalDevice, type_flag, properties::MemoryPropertyFlag)
     mem_props = get_physical_device_memory_properties(physical_device)
     indices = findall(x -> (x.property_flags & properties) == properties, mem_props.memory_types[1:mem_props.memory_type_count]) .- 1
@@ -126,10 +124,11 @@ function main()
     framebuffer = Framebuffer(render_pass.device, render_pass, [fb_image_view], width, height, 1)
 
     # prepare shaders
-    vert_shader = Shader(device, ShaderFile(joinpath(@__DIR__, "triangle.vert"), FormatGLSL()), [DescriptorBinding(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 0, 0)])
+    vert_shader = Shader(device, ShaderFile(joinpath(@__DIR__, "triangle.vert"), FormatGLSL()), DescriptorBinding[])
     vertices = pos_color(ps, colors)
     frag_shader = Shader(device, ShaderFile(joinpath(@__DIR__, "triangle.frag"), FormatGLSL()), DescriptorBinding[])
     shaders = [vert_shader, frag_shader]
+    @assert isempty(descriptor_set_layouts(shaders))
 
     # build graphics pipeline
     shader_stage_cis = PipelineShaderStageCreateInfo.(shaders)
@@ -138,14 +137,15 @@ function main()
     viewport_state = PipelineViewportStateCreateInfo(viewports=[Viewport(0, 0, width, height, 0, 1)], scissors=[Rect2D(Offset2D(0,0),Extent2D(width, height))])
     rasterizer = PipelineRasterizationStateCreateInfo(false, false, VK_POLYGON_MODE_FILL, VK_FRONT_FACE_CLOCKWISE, false, 0., 0., 0., 1., cull_mode=CULL_MODE_BACK_BIT)
     multisample_state = PipelineMultisampleStateCreateInfo(SAMPLE_COUNT_1_BIT, false, 1., false, false)
-    color_blend_attachment = PipelineColorBlendAttachmentState(false, VK_BLEND_FACTOR_ONE, VK_BLEND_FACTOR_ZERO, VK_BLEND_OP_ADD, VK_BLEND_FACTOR_ONE, VK_BLEND_FACTOR_ZERO, VK_BLEND_OP_ADD)
-    color_blend_state = PipelineColorBlendStateCreateInfo(false, VK_LOGIC_OP_CLEAR, [color_blend_attachment], Float32.((0.0, 1.0, 1.0, 0.01)))
+    color_blend_attachment = PipelineColorBlendAttachmentState(true, VK_BLEND_FACTOR_ONE, VK_BLEND_FACTOR_ZERO, VK_BLEND_OP_ADD, VK_BLEND_FACTOR_SRC_ALPHA, VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA, VK_BLEND_OP_ADD; color_write_mask = COLOR_COMPONENT_R_BIT | COLOR_COMPONENT_G_BIT | COLOR_COMPONENT_B_BIT | COLOR_COMPONENT_A_BIT)
+    color_blend_state = PipelineColorBlendStateCreateInfo(false, VK_LOGIC_OP_COPY, [color_blend_attachment], Float32.((0., 0., 0., 0.)))
+    pipeline_layout = PipelineLayout(device, [], [])
     (pipeline, _...), _ = unwrap(create_graphics_pipelines(
         device,
         [GraphicsPipelineCreateInfo(
             shader_stage_cis,
             rasterizer,
-            PipelineLayout(device, descriptor_set_layouts(shaders), []),
+            pipeline_layout,
             render_pass,
             0,
             0;
@@ -167,16 +167,6 @@ function main()
     unsafe_copyto!(Ptr{eltype(vertices)}(dataptr), pointer(vertices), length(vertices))
     unwrap(unmap_memory(vertex_buffer_memory.device, vertex_buffer_memory))
 
-    # prepare index buffer
-    datasize = sizeof(eltype(vertex_indices)) * length(vertex_indices)
-    index_buffer = Buffer(device, datasize, BUFFER_USAGE_INDEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE, [0])
-    index_buffer_memreqs = get_buffer_memory_requirements(index_buffer.device, index_buffer)
-    index_buffer_memory = DeviceMemory(index_buffer.device, index_buffer_memreqs, MEMORY_PROPERTY_HOST_VISIBLE_BIT | MEMORY_PROPERTY_HOST_COHERENT_BIT)
-    bind_buffer_memory(index_buffer.device, index_buffer, index_buffer_memory, 0)
-    dataptr = unwrap(map_memory(index_buffer_memory.device, index_buffer_memory, 0, datasize))
-    unsafe_copyto!(Ptr{eltype(vertex_indices)}(dataptr), pointer(vertex_indices), length(vertex_indices))
-    unwrap(unmap_memory(index_buffer_memory.device, index_buffer_memory))
-
     # prepare transfer of rendered image
     local_image = Image(device, VK_IMAGE_TYPE_2D, format, Extent3D(width, height, 1), 1, 1, SAMPLE_COUNT_1_BIT, VK_IMAGE_TILING_LINEAR, IMAGE_USAGE_TRANSFER_DST_BIT, VK_SHARING_MODE_EXCLUSIVE, [0], VK_IMAGE_LAYOUT_UNDEFINED)
     local_image_memreqs = get_image_memory_requirements(local_image.device, local_image)
@@ -187,11 +177,10 @@ function main()
     command_buffer, _... = unwrap(allocate_command_buffers(device, CommandBufferAllocateInfo(command_pool, VK_COMMAND_BUFFER_LEVEL_PRIMARY, 1)))
     begin_command_buffer(command_buffer, CommandBufferBeginInfo())
     cmd_bind_vertex_buffers(command_buffer, [vertex_buffer], [0])
-    cmd_bind_index_buffer(command_buffer, index_buffer, 0, VK_INDEX_TYPE_UINT32)
     cmd_bind_pipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline)
     cmd_pipeline_barrier(command_buffer, PIPELINE_STAGE_TOP_OF_PIPE_BIT, PIPELINE_STAGE_TRANSFER_BIT, [], [], [ImageMemoryBarrier(AccessFlag(0), ACCESS_MEMORY_READ_BIT, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_QUEUE_FAMILY_IGNORED, VK_QUEUE_FAMILY_IGNORED, local_image, ImageSubresourceRange(IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1))])
-    cmd_begin_render_pass(command_buffer, RenderPassBeginInfo(render_pass, framebuffer, Rect2D(Offset2D(0,0),Extent2D(width, height)), [vk.VkClearValue(vk.VkClearColorValue(tuple(rand(Float16, 3)..., 1f0)))]), VK_SUBPASS_CONTENTS_INLINE)
-    cmd_draw_indexed(command_buffer, 4, 1, 0, 0, 0)
+    cmd_begin_render_pass(command_buffer, RenderPassBeginInfo(render_pass, framebuffer, Rect2D(Offset2D(0,0),Extent2D(width, height)), [vk.VkClearValue(vk.VkClearColorValue((0.1f0,0.1f0,0.15f0,1f0)))]), VK_SUBPASS_CONTENTS_INLINE)
+    cmd_draw(command_buffer, 4, 1, 0, 0)
     cmd_end_render_pass(command_buffer)
     cmd_pipeline_barrier(command_buffer, PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, PIPELINE_STAGE_TRANSFER_BIT, [], [], [ImageMemoryBarrier(ACCESS_COLOR_ATTACHMENT_WRITE_BIT, ACCESS_MEMORY_READ_BIT, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, VK_QUEUE_FAMILY_IGNORED, VK_QUEUE_FAMILY_IGNORED, fb_image, ImageSubresourceRange(IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1))])
     cmd_copy_image(command_buffer, fb_image, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, local_image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
