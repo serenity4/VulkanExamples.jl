0029fe1bb093206686d144d2e39e45e0ee46d604
serenity4 <cedric.bel@hotmail.fr>
2021-03-14 23:28:01 +0100

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,28 @@ using FileIO
     @testset "Headless" begin
         include("../examples/headless/headless.jl")
         dst = tempname() * ".png"
-        main(dst)
+        main(
+            joinpath(@__DIR__, "render.png"),
+            Point2f[(-0.5, -0.5), (0.5, -0.5), (0.5, 0.5), (-0.5, 0.5)],
+            [
+                RGBA{Float16}(0.0, 0.0, 1.0, 0.0),
+                RGBA{Float16}(0.0, 1.0, 0.0, 0.05),
+                RGBA{Float16}(1.0, 1.0, 1.0, 0.4),
+                RGBA{Float16}(1.0, 0.0, 0.0, 1.0),
+            ],
+        )
+        @test stat(dst).size > 1
+    end
+    @testset "Texture" begin
+        include("../examples/texture/texture_2d.jl")
+        dst = tempname() * ".png"
+        main(
+            joinpath(@__DIR__, "render.png"),
+            Point2f[(-1., -1.), (1., -1.), (-1., 1.), (1., 1.)],
+            joinpath(@__DIR__, "texture_2d.png"),
+            width = 1024,
+            height = 1024,
+        )
         @test stat(dst).size > 1
     end
 end
